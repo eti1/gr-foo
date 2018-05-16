a0f378cbb305143a0c58cab9d75852f445bfbcf5
etienne <etiennehelluy@gmail.com>
2018-05-16 17:26:38 +0200

    free some cpu time

--- lib/wireshark_connector_impl.cc
+++ lib/wireshark_connector_impl.cc
@@ -181,7 +181,7 @@ wireshark_connector_impl::general_work(int noutput, gr_vector_int& ninput_items,
 	gr_complex *out = (gr_complex*)output_items[0];
 
 	if(!d_msg_len) {
-		pmt::pmt_t msg(delete_head_nowait(pmt::mp("in")));
+		pmt::pmt_t msg(delete_head_blocking(pmt::mp("in")));
 
 		if(!msg) {
 			return 0;
